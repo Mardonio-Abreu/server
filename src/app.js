678467b006369cdd405a194f3a8adbac42d5e58a
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-05-26 17:57:12 -0600

    nodemon added to the json file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-const express = require ('express');
+import express from  'express';
+import pkg from '../classes/productManager.cjs';
 
+const { ProductManager } = pkg;
 const app = express();
+
+
 
 app.get('/saludo', (req, res)=>{res.send("Surprise MotherFather")})
 
-app.listen(8080, () => console.log("Surprise MotherFather on port 8080"))
+app.listen(8080, () => console.log("Surprise MotherFather on port 8080!"))
